dde80ffe42a9cf9ea3ca9d7e730efbf7315de574
claired4l <115007761+claired4l@users.noreply.github.com>
2025-02-22 16:56:57 +0100

    use Scanner constructor supported since Android Api Level 1 and add tests for UTF-8 values (#86)

--- src/main/java/io/github/cdimascio/dotenv/internal/ClasspathHelper.java
+++ src/main/java/io/github/cdimascio/dotenv/internal/ClasspathHelper.java
@@ -25,12 +25,12 @@ public class ClasspathHelper {
             throw new DotenvException("Could not find "+location+" on the classpath");
         }
 
-        final var scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
+        final var scanner = new Scanner(inputStream, "utf-8");
         final var lines = new ArrayList<String>();
         while (scanner.hasNext()) {
             lines.add(scanner.nextLine());
         }
-
+        scanner.close();
         return lines.stream();
     }
 }
